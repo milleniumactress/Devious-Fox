67bbd34e15a4a79b5af692b914ed51adf6371650
richardwijaya <32352818+richardwijaya@users.noreply.github.com>
2017-11-11 18:55:35 +0700

    Create CurrentScore.java

---
+++ ByronicFox/src/mygame/CurrentScore.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * @author Richard
+ */
+public class CurrentScore extends Score{
+  
+  public Score(){
+    score = 0;
+  }
+  
+  public int getScore(){
+    score++;
+  }
+  
+  public int updateScore(){
+    score++;
+  }
+}
